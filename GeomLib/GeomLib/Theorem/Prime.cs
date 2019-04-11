400184621554e85100b5e369f8c4486c61f3bdc8
Tejas <tejaskatakdhond@gmail.com>
2019-04-11 23:49:12 +0530

    - prime number identification

---
+++ GeomLib/GeomLib/Theorem/Prime.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeomLib.Theorem
+{
+    class Prime
+    {
+        /// <summary>
+        /// check if the number is prime number
+        /// </summary>
+        /// <returns>true if the number is a prime number</returns>
+        public bool CheckIfNumberIsPrime(int number)
+        {
+            var isPrime = true;
+
+            try
+            {
+                for (var i = 2; i <= number / 2; i++)
+                {
+                    if (0 == number % i)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                isPrime = false;
+            }
+            return isPrime;
+        }
+    }
+}
